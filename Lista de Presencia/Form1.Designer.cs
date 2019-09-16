4e4c72f9f378fce974f0cec845e25675e4b1cde1
Vico-MANS <55394543+Vico-MANS@users.noreply.github.com>
2019-09-16 16:38:42 +0200

    Human insertion and deletion working

--- Lista de Presencia/Form1.Designer.cs
+++ Lista de Presencia/Form1.Designer.cs
@@ -31,11 +31,26 @@
             this.components = new System.ComponentModel.Container();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAddPerson = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabOverview = new System.Windows.Forms.TabPage();
+            this.btnDeleteSelected = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPresence = new System.Windows.Forms.TabPage();
+            this.colFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colBirthday = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabOverview.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -49,58 +64,173 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.colFirstName,
+            this.colLastName,
+            this.colBirthday});
+            this.dataGridView1.Location = new System.Drawing.Point(20, 9);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 301);
+            this.dataGridView1.Size = new System.Drawing.Size(722, 301);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // button1
+            // btnAddPerson
             // 
-            this.button1.Location = new System.Drawing.Point(279, 349);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 40);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Refresh";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnAddPerson.Location = new System.Drawing.Point(567, 373);
+            this.btnAddPerson.Name = "btnAddPerson";
+            this.btnAddPerson.Size = new System.Drawing.Size(86, 29);
+            this.btnAddPerson.TabIndex = 2;
+            this.btnAddPerson.Text = "Add Person";
+            this.btnAddPerson.UseVisualStyleBackColor = true;
+            this.btnAddPerson.Click += new System.EventHandler(this.btnAddPerson_Click);
             // 
-            // Column1
+            // splitContainer1
             // 
-            this.Column1.HeaderText = "FIRSTNAME";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.Width = 225;
+            this.splitContainer1.Location = new System.Drawing.Point(218, 340);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(54, 19);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 3;
             // 
-            // Column2
+            // tabControl1
             // 
-            this.Column2.HeaderText = "LASTNAME";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            this.Column2.Width = 225;
+            this.tabControl1.Controls.Add(this.tabOverview);
+            this.tabControl1.Controls.Add(this.tabPresence);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(787, 436);
+            this.tabControl1.TabIndex = 4;
             // 
-            // Column3
+            // tabOverview
             // 
-            this.Column3.HeaderText = "BIRTHDAY";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            this.Column3.Width = 225;
+            this.tabOverview.Controls.Add(this.btnDeleteSelected);
+            this.tabOverview.Controls.Add(this.label3);
+            this.tabOverview.Controls.Add(this.dtpBirthday);
+            this.tabOverview.Controls.Add(this.txtLastname);
+            this.tabOverview.Controls.Add(this.label2);
+            this.tabOverview.Controls.Add(this.txtFirstname);
+            this.tabOverview.Controls.Add(this.label1);
+            this.tabOverview.Controls.Add(this.dataGridView1);
+            this.tabOverview.Controls.Add(this.btnAddPerson);
+            this.tabOverview.Location = new System.Drawing.Point(4, 22);
+            this.tabOverview.Name = "tabOverview";
+            this.tabOverview.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOverview.Size = new System.Drawing.Size(779, 410);
+            this.tabOverview.TabIndex = 0;
+            this.tabOverview.Text = "Overview";
+            this.tabOverview.UseVisualStyleBackColor = true;
+            this.tabOverview.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // btnDeleteSelected
+            // 
+            this.btnDeleteSelected.Location = new System.Drawing.Point(608, 316);
+            this.btnDeleteSelected.Name = "btnDeleteSelected";
+            this.btnDeleteSelected.Size = new System.Drawing.Size(134, 23);
+            this.btnDeleteSelected.TabIndex = 9;
+            this.btnDeleteSelected.Text = "Delete selected rows";
+            this.btnDeleteSelected.UseVisualStyleBackColor = true;
+            this.btnDeleteSelected.Click += new System.EventHandler(this.btn_DeleteSelected);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Birthday";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.Location = new System.Drawing.Point(442, 378);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(99, 20);
+            this.dtpBirthday.TabIndex = 7;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(269, 378);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(99, 20);
+            this.txtLastname.TabIndex = 6;
+            this.txtLastname.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last name";
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(106, 378);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(77, 20);
+            this.txtFirstname.TabIndex = 4;
+            this.txtFirstname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 381);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabPresence
+            // 
+            this.tabPresence.Location = new System.Drawing.Point(4, 22);
+            this.tabPresence.Name = "tabPresence";
+            this.tabPresence.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPresence.Size = new System.Drawing.Size(779, 410);
+            this.tabPresence.TabIndex = 1;
+            this.tabPresence.Text = "Presence";
+            this.tabPresence.UseVisualStyleBackColor = true;
+            // 
+            // colFirstName
+            // 
+            this.colFirstName.HeaderText = "FIRSTNAME";
+            this.colFirstName.Name = "colFirstName";
+            this.colFirstName.ReadOnly = true;
+            this.colFirstName.Width = 225;
+            // 
+            // colLastName
+            // 
+            this.colLastName.HeaderText = "LASTNAME";
+            this.colLastName.Name = "colLastName";
+            this.colLastName.ReadOnly = true;
+            this.colLastName.Width = 225;
+            // 
+            // colBirthday
+            // 
+            this.colBirthday.HeaderText = "BIRTHDAY";
+            this.colBirthday.Name = "colBirthday";
+            this.colBirthday.ReadOnly = true;
+            this.colBirthday.Width = 225;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabOverview.ResumeLayout(false);
+            this.tabOverview.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -109,10 +239,21 @@
 
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btnAddPerson;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabOverview;
+        private System.Windows.Forms.TabPage tabPresence;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDeleteSelected;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colBirthday;
     }
 }
 
--- Lista de Presencia/Form1.cs
+++ Lista de Presencia/Form1.cs
@@ -20,20 +20,20 @@ namespace Lista_de_Presencia
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            GetPersonData();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void GetPersonData()
         {
             dataGridView1.Rows.Clear();
-            
+
             using (SqlConnection conn = new SqlConnection())
             {
                 conn.ConnectionString = "Server=USUARIO-PC\\SQLEXPRESS;Database=MALM;Trusted_Connection=true";
                 conn.Open();
                 Console.WriteLine("Connection opened...\n");
 
-                SqlCommand command = new SqlCommand("SELECT FIRSTNAME, LASTNAME, (SELECT CONVERT(varchar(10), BIRTHDAY, 103) AS [DD/MM/YYYY]) AS BIRTHDAY FROM Person", conn);
+                SqlCommand command = new SqlCommand("SELECT FIRSTNAME, LASTNAME, (SELECT CONVERT(varchar(10), BIRTHDAY, 103) AS [DD/MM/YYYY]) AS BIRTHDAY FROM Human", conn);
 
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
@@ -47,7 +47,73 @@ namespace Lista_de_Presencia
             }
         }
 
+        private void btnAddPerson_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Add Person");
+            
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=USUARIO-PC\\SQLEXPRESS;Database=MALM;Trusted_Connection=true";
+                conn.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Human (FIRSTNAME, LASTNAME, BIRTHDAY) VALUES (@firstname, @lastname, @birthday)", conn);
+                command.Parameters.Add(new SqlParameter("firstname", txtFirstname.Text));
+                command.Parameters.Add(new SqlParameter("lastname", txtLastname.Text));
+                command.Parameters.Add(new SqlParameter("birthday", dtpBirthday.Value));
+
+                Console.WriteLine("Insert affected " + command.ExecuteNonQuery() + " rows.");
+
+                GetPersonData();
+            }
+        }
+
+        private void btn_DeleteSelected(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=USUARIO-PC\\SQLEXPRESS;Database=MALM;Trusted_Connection=true";
+                conn.Open();
+
+                int deletionCounter = 0;
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    SqlCommand command = new SqlCommand("DELETE FROM Human WHERE FIRSTNAME = @firstname AND LASTNAME = @lastname", conn);
+                    command.Parameters.Add(new SqlParameter("firstname", row.Cells["colFirstName"].Value.ToString()));
+                    command.Parameters.Add(new SqlParameter("lastname", row.Cells["colLastName"].Value.ToString()));
+
+                    deletionCounter += command.ExecuteNonQuery();
+                }
+
+                Console.WriteLine(deletionCounter + " rows where deleted.");
+                GetPersonData();
+            }
+        }
+
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
